68da42317d7ff9a2f6e0270e3b47904c5f5d7ad6
thelonelywolf88@gmail.com <thelonelywolf88@gmail.com>
2012-12-15 11:47:19 +0000

    Helper actualizado

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,4 +19,10 @@ end
 
 Rspec.configure do |config|
     config.include Rack::Test::Methods
+    config.use_transactional_fixtures = true
+    config.infer_base_class_for_anonymous_controllers = false
+
+    def test_sign_in(user)
+        controller.sign_in(user)
+    end
 end
