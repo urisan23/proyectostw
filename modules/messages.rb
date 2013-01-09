4dc066b0ed1d0ffd2a1b164531953b92cb587944
urisan23 <urisan91@gmail.com>
2013-01-09 00:02:32 +0000

    Contador de mensajes sin leer

--- modules/messages.rb
+++ modules/messages.rb
@@ -1,11 +1,17 @@
 get '/inbox' do
+  auxid = session[:current_user].id
+  session.clear
   ids = []
-  session[:current_user].messages.each{|m|
+  User.get(auxid).messages.each{|m|
     ids << m[:to]
     ids << m[:from]
+    m.read = TRUE
+    m.save
   }
+  User.get(auxid).save  
   ids.uniq!
-  ids.delete_if {|x| x == session[:current_user].id}
+  ids.delete_if {|x| x == User.get(auxid).id}
+  session[:current_user] = User.get(auxid)
   haml :inbox, :locals => { :us => session[:current_user], :msgs => session[:current_user].messages, :users => User.all, :ids => ids}
 end
 
@@ -19,6 +25,7 @@ post '/send_message/:id' do|id|
   msg1.from = sender.id
   msg1.to = receiver.id
   msg1.time = Time.now
+  msg1.read = TRUE
   msg1.save
   msg2 = Message.new
   msg2.body = params[:body]
